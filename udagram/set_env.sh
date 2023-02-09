43dea207a32aa12f288e282df8ac6963631e9737
KarimAbdelnasser <kareemabdo38@yahoo.com>
2023-02-09 09:48:27 +0200

    first commit

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=Zika1234
+export POSTGRES_HOST=localhost
+export POSTGRES_DB=udagram
+export AWS_BUCKET=udagram0113233
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=tryAgain
+export URL=http://localhost:4200
