e1dcedeb87c3015a6cfa73f3629675b871988d73
KarimAbdelnasser <kareemabdo38@yahoo.com>
2023-02-12 18:02:20 +0200

    first commit

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -8,4 +8,4 @@ export AWS_BUCKET=udagram03211230
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=tryAgain
-export URL=http://localhost:4200
+export URL=http://udagram03211230.s3-website-us-east-1.amazonaws.com
